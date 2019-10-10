71c0f35f602f562508094641db209601c9193331
Fred Morrison <fpmorrison@verizon.net>
2019-10-10 15:25:31 -0400

    laptop main updates

--- PictureCompressor/Program.cs
+++ PictureCompressor/Program.cs
@@ -12,10 +12,18 @@ namespace PictureCompressor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start");
-            const string INPUT_PATH = @"S:\Cats\ValleyVet stuff";
-            const string OUTPUT_PATH = @"S:\Cats\ValleyVet stuff\small33";
             const int COMPRESSION_QUALITY = 33;
+            const string INPUT_PATH = @"C:\Users\fpmor\OneDrive\Concrete Steps Repair";
+            Console.WriteLine("Start");
+            
+            // C:\Users\fpmor\OneDrive\2019 Driveway Pictures
+            
+            string OUTPUT_PATH = Path.Combine(INPUT_PATH, COMPRESSION_QUALITY.ToString());
+            if (!Directory.Exists(OUTPUT_PATH)) 
+            {
+                Directory.CreateDirectory(OUTPUT_PATH);
+            }
+            
 
             if (Directory.Exists(INPUT_PATH) && Directory.Exists(OUTPUT_PATH))
             {
@@ -30,7 +38,7 @@ namespace PictureCompressor
             }
             else
             {
-                Console.WriteLine("Either the input directory or output directory or both doesn't exist.");
+                Console.WriteLine($"Either the input directory {INPUT_PATH} does not exist or output directory {OUTPUT_PATH} does not exits or both don't exist.");
             }
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
