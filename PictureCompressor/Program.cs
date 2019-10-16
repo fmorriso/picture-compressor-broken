640b37afb9a8ce825392a8a9f90f22458fe4e960
Fred Morrison <fpmorrison@verizon.net>
2019-10-15 21:07:36 -0400

    change directory

--- PictureCompressor/Program.cs
+++ PictureCompressor/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
 //
 
 //
@@ -17,26 +18,41 @@ namespace PictureCompressor
             // "C:\Users\fpmor\OneDrive\travel\2019-10-Elkins-WV\pictures\mls pics"
             const string INPUT_PATH = @"C:\Users\fpmor\OneDrive\travel\2019-10-Elkins-WV\pictures\originals";
             Console.WriteLine("Start");
-            
+
+
+
+/*            string folderPath = "";
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                
+                //folderBrowser.RootFolder = @"C:\Users\fpmor\OneDrive\";
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    folderPath = folderBrowser.SelectedPath;
+                }
+            }*/
+
+
+
             // C:\Users\fpmor\OneDrive\2019 Driveway Pictures
-            
+
             string OUTPUT_PATH = Path.Combine(INPUT_PATH, COMPRESSION_QUALITY.ToString());
-            if (!Directory.Exists(OUTPUT_PATH)) 
+            if (!Directory.Exists(OUTPUT_PATH))
             {
                 Directory.CreateDirectory(OUTPUT_PATH);
             }
-            
+
 
             if (Directory.Exists(INPUT_PATH) && Directory.Exists(OUTPUT_PATH))
             {
                 var di = new DirectoryInfo(INPUT_PATH);
-                foreach (Image file in di.GetFiles("*.jpg")
+                foreach (Image file in di.GetFiles(" *.jpg")
                                          .Select(f => new { f, outputFilename = Path.Combine(OUTPUT_PATH, f.Name) })
                                          .Where(@t => !File.Exists(@t.outputFilename))
                                          .Select(@t => ImageUtilities.ImageHelper.SaveCompressedJpegImage(@t.f.FullName, @t.outputFilename, COMPRESSION_QUALITY)))
                 {
                     file.Dispose();
-                } 
+                }
             }
             else
             {
